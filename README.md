a3628fc263fb6030746f9ecb1faee6d3ef7b14ed
Richard Weber <richard.weber@nitor.com>
2017-11-30 22:29:40 +0000

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gitref to Serverless stage
+
+A simple plugin that takes your current git branch name (or commit hash if in detached HEAD) and uses that as the stage name for deployments.
+
+
+If you want to override this behaviour use the shorthand "-s" flag to override.
---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const {exec} = require('child_process');
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.handler = this.handler.bind(this);
+    
+    this.hooks = {
+      'before:deploy:deploy': this.configureStage.bind(this),
+      'before:remove:remove': this.configureStage.bind(this),
+    };
+  }
+
+  log(message) {
+    this.serverless.cli.log("Branch2Stage: " + message);
+  }
+
+  handler(err, stdout, stderr) {
+    if (err) {
+      this.serverless.cli.log(stderr);
+    }
+  
+    let ref = stdout.substring(0, stdout.length - 1);
+  
+    if (ref !== "HEAD") {
+      this.log("Changing stage to: " + ref + " based on current git HEAD");
+      this.options.stage = ref;
+    }
+    else {
+      exec("git rev-parse HEAD", this.handler);
+    }
+  }
+  
+  configureStage() {
+    if(typeof this.options.s !== "undefined") {
+      this.log("Stage set by commandline option (-s), skipping...");
+    } else {
+      exec("git rev-parse --abbrev-ref HEAD", this.handler);  
+    }
+  }
+  
+}
+
+module.exports = ServerlessPlugin;
